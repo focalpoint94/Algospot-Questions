4359e099a41acbe963189dce4a44b3a46c4cb920
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2019-09-13 14:30:37 +0900

    Boggle
    Recursion

--- main.cpp
+++ main.cpp
@@ -10,37 +10,92 @@
 
 using namespace std;
 
-void print_vec(const vector<int>& vec) {
-	for (auto i = vec.begin(); i != vec.end(); ++i) {
-		cout << *i << " ";
+// Boggle 보드
+char board[5][5];
+// 8방향
+const int dx[8] = { -1, -1, -1, 1, 1, 1, 0, 0 };
+const int dy[8] = { -1, 0, 1, -1, 0, 1, -1, 1 };
+// 탐색 유효성
+int valid[5][5][10];
+
+void print_board() {
+	cout << endl << "*Boggle*" << endl;
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 5; ++j)
+			cout << board[i][j] << " ";
+		cout << endl;
 	}
-	cout << endl;
 }
 
-/*
-// 이 함수는 0~n개의 원소를 선택하여 a1<a2<a3<a4를 만족하는 경우를 모두 출력
-// n: 0~n개의 원소 중 선택
-// vec: 순서쌍의 저장
-// to_pick: 앞으로 선택해야 할 숫자의 갯수
-*/
-void picking(int n, vector<int>& vec, int to_pick) {
-	// 뽑을 것이 없는 경우
-	if (to_pick == 0) {
-		print_vec(vec);
-		return;
+void set_valid_to_one() {
+	for (int i = 0; i < 5; ++i)
+		for (int j = 0; j < 5; ++j)
+			for (int k = 0; k < 10; ++k)
+				valid[i][j][k] = 1;
+}
+
+bool is_word_by_idx(int x, int y, const string& word, int idx) {
+
+	// 유효한 좌표인지 확인
+	if (x >= 5 || x < 0 || y >= 5 || y < 0)
+		return false;
+	// 현재 좌표의 글자가 불일치하는지 확인
+	if (word[idx] != board[x][y])
+	{
+		valid[x][y][idx] = 0;
+		return false;
 	}
-	int min = to_pick == 4 ? 0 : vec.back() + 1;
-	for (int i = min; i <= n; ++i) {
-		vec.push_back(i);
-		picking(n, vec, to_pick - 1);
-		vec.pop_back();
+	// 글자가 일치한 경우, 마지막 글자였다면 성공
+	if (idx + 1 == word.size())
+		return true;
+
+	// 글자가 남아있다면, 8방향을 모두 확인
+	for (int i = 0; i < 8; ++i) {
+		// 다음 좌표 설정
+		int x_next = x + dx[i];
+		int y_next = y + dy[i];
+		// 이전 탐색 기록을 바탕으로, 불필요한 탐색이라면 skip
+		if (valid[x_next][y_next][idx + 1] == 0)
+			continue;
+		// 뒤의 모든 글자가 일치하여 true가 return되었다면, 성공
+		if (is_word_by_idx(x_next, y_next, word, idx + 1))
+			return true;
 	}
+	return false;
+}
+
+bool is_word(const string& word) {
+	set_valid_to_one();
+	for (int i = 0; i < 5; ++i)
+		for (int j = 0; j < 5; ++j)
+			if (is_word_by_idx(i, j, word, 0) == true)
+				return true;
+	return false;
 }
 
 int main()
 {
-	vector<int> vec;
-	picking(9, vec, 4);
-
+	int cases;
+	cin >> cases;
+	int count = 0;
+	while (count < cases) {
+		for (int i = 0; i < 5; ++i)
+			for (int j = 0; j < 5; ++j)
+				cin >> board[i][j];
+		int num_tests;
+		cin >> num_tests;
+		string *words = new string[num_tests];
+		for (int i = 0; i < num_tests; ++i) {
+			cin >> words[i];
+		}
+		for (int i = 0; i < num_tests; ++i) {
+			if (is_word(words[i]))
+				cout << words[i] << " " << "YES" << endl;
+			else
+				cout << words[i] << " " << "NO" << endl;
+		}
+		delete[] words;
+		count++;
+	}
 	return 0;
 }
