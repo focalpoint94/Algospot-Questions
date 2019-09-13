2384c98206415fa8ff71d90163f7f27cd0bf0a51
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2019-09-13 14:31:02 +0900

    Picnic

--- main.cpp
+++ main.cpp
@@ -10,92 +10,82 @@
 
 using namespace std;
 
-// Boggle 보드
-char board[5][5];
-// 8방향
-const int dx[8] = { -1, -1, -1, 1, 1, 1, 0, 0 };
-const int dy[8] = { -1, 0, 1, -1, 0, 1, -1, 1 };
-// 탐색 유효성
-int valid[5][5][10];
+int answer;
+int taken[10];
 
-void print_board() {
-	cout << endl << "*Boggle*" << endl;
-	for (int i = 0; i < 5; ++i) {
-		for (int j = 0; j < 5; ++j)
-			cout << board[i][j] << " ";
+void print_friend_matrix(int **arr, int n) {
+	cout << "Friend Matrix" << endl;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j)
+			cout << arr[i][j] << " ";
 		cout << endl;
 	}
 }
 
-void set_valid_to_one() {
-	for (int i = 0; i < 5; ++i)
-		for (int j = 0; j < 5; ++j)
-			for (int k = 0; k < 10; ++k)
-				valid[i][j][k] = 1;
+void set_zero() {
+	answer = 0;
+	for (int i = 0; i < 10; ++i)
+		taken[i] = 0;
 }
 
-bool is_word_by_idx(int x, int y, const string& word, int idx) {
-
-	// 유효한 좌표인지 확인
-	if (x >= 5 || x < 0 || y >= 5 || y < 0)
-		return false;
-	// 현재 좌표의 글자가 불일치하는지 확인
-	if (word[idx] != board[x][y])
-	{
-		valid[x][y][idx] = 0;
-		return false;
+void count_pairs(int **arr, int n) {
+	int min_idx = -1;
+	for (int i = 0; i < n; ++i) {
+		if (taken[i] == 0) {
+			min_idx = i;
+			break;
+		}
 	}
-	// 글자가 일치한 경우, 마지막 글자였다면 성공
-	if (idx + 1 == word.size())
-		return true;
-
-	// 글자가 남아있다면, 8방향을 모두 확인
-	for (int i = 0; i < 8; ++i) {
-		// 다음 좌표 설정
-		int x_next = x + dx[i];
-		int y_next = y + dy[i];
-		// 이전 탐색 기록을 바탕으로, 불필요한 탐색이라면 skip
-		if (valid[x_next][y_next][idx + 1] == 0)
-			continue;
-		// 뒤의 모든 글자가 일치하여 true가 return되었다면, 성공
-		if (is_word_by_idx(x_next, y_next, word, idx + 1))
-			return true;
+	if (min_idx == -1) {
+		answer++;
+		return;
 	}
-	return false;
-}
-
-bool is_word(const string& word) {
-	set_valid_to_one();
-	for (int i = 0; i < 5; ++i)
-		for (int j = 0; j < 5; ++j)
-			if (is_word_by_idx(i, j, word, 0) == true)
-				return true;
-	return false;
+	for (int i = min_idx + 1; i < n; ++i) {
+		if (arr[min_idx][i] == 1 && taken[i] == 0) {
+			taken[min_idx] = taken[i] = 1;
+			count_pairs(arr, n);
+			taken[min_idx] = taken[i] = 0;
+		}
+	}
 }
 
 int main()
 {
+	vector<int> answers;
 	int cases;
 	cin >> cases;
 	int count = 0;
 	while (count < cases) {
-		for (int i = 0; i < 5; ++i)
-			for (int j = 0; j < 5; ++j)
-				cin >> board[i][j];
-		int num_tests;
-		cin >> num_tests;
-		string *words = new string[num_tests];
-		for (int i = 0; i < num_tests; ++i) {
-			cin >> words[i];
+		// n: 학생의 수
+		int n;
+		cin >> n;
+		// pairs: 친구 쌍의 수
+		int pairs;
+		cin >> pairs;
+		// 친구 관계 기록 배열
+		int **arr = new int*[n];
+		for (int i = 0; i < n; ++i)
+			arr[i] = new int[n];
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+				arr[i][j] = 0;
+		int check = 0;
+		while (check < pairs) {
+			int temp1, temp2;
+			cin >> temp1 >> temp2;
+			arr[temp1][temp2] = 1;
+			arr[temp2][temp1] = 1;
+			check++;
 		}
-		for (int i = 0; i < num_tests; ++i) {
-			if (is_word(words[i]))
-				cout << words[i] << " " << "YES" << endl;
-			else
-				cout << words[i] << " " << "NO" << endl;
-		}
-		delete[] words;
+		set_zero();
+		count_pairs(arr, n);
+		answers.push_back(answer);
+		for (int i = 0; i < n; ++i)
+			delete[] arr[i];
+		delete[] arr;
 		count++;
 	}
+	for (auto i = answers.begin(); i != answers.end(); ++i)
+		cout << *i << endl;
 	return 0;
 }
