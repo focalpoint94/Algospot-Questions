24a0864b10c236022343bebe6a340f99d108eefb
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2019-10-06 00:12:34 +0900

    Update lis.cpp

--- lis.cpp
+++ lis.cpp
@@ -5,64 +5,61 @@
 #include <array>
 #include <vector>
 #include <tuple>
+#include <set>
+#include <unordered_set>
 #include <map>
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <sstream>
 #include <string>
+#include <cstring>
 #include <algorithm>
+#include <utility>
+#include <cmath>
 #include <limits>
 
 using namespace std;
 
 int N;
-int arr[500] = { 0, };
+int num[500];
+int cache[501];
 
-int len[500] = { 0, };
-int prev_idx[500] = { 0, };
-
-void set() {
-	for (int i = 0; i < 500; ++i)
-		arr[i] = len[i] = prev_idx[i] = 0;
+int LIS_sub(int start) {
+	if (start == N) return 0;
+	int& ret = cache[start];
+	if (ret != -1) return ret;
+	ret = 1;
+	for (int i = start + 1; i < N; ++i) {
+		if (num[i] > num[start]) {
+			ret = max(ret, LIS_sub(i) + 1);
+		}
+	}
+	return ret;
 }
 
 int LIS() {
-	len[0] = 1;
-	prev_idx[0] = -1;
-	for (int i = 1; i < N; ++i) {
-		int prev_ = -1;
-		int max_ = -1;
-		for (int j = 0; j < i; ++j) {
-			if (arr[j] < arr[i] && len[j] > max_) {
-				max_ = len[j];
-				prev_ = j;
-			}
-		}
-		if (prev_ == -1) len[i] = 1;
-		else len[i] = len[prev_] + 1;
-		prev_idx[i] = prev_;
-	}
-	int max = -1;
-	for (int i = 0; i < N; ++i) {
-		if (len[i] > max)
-			max = len[i];
-	}
-	return max;
+	int ret = 0;
+	for (int i = 0; i < N; ++i)
+		ret = max(ret, LIS_sub(i));
+	return ret;
 }
 
 int main()
 {
-	vector<int> answers;
+	vector<int> ans;
 	int cases;
 	cin >> cases;
 	int count = 0;
 	while (count < cases) {
-		set();
 		cin >> N;
-		for (int i = 0; i < N; ++i)
-			cin >> arr[i];
-		int ans = LIS();
-		answers.push_back(ans);
+		for (int i = 0; i < N; ++i) cin >> num[i];
+		memset(cache, -1, sizeof(cache));
+		ans.push_back(LIS());
 		count++;
 	}
-	for (int i = 0; i < answers.size(); ++i)
-		cout << answers[i] << endl;
+	for (auto i = ans.begin(); i != ans.end(); ++i)
+		cout << *i << endl;
 	return 0;
 }
