2c60533e8b30234dc8fef8c99a39dc87974e859e
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2019-10-06 22:46:23 +0900

    Update jlis.cpp

--- jlis.cpp
+++ jlis.cpp
@@ -5,42 +5,45 @@
 #include <array>
 #include <vector>
 #include <tuple>
+#include <set>
+#include <unordered_set>
 #include <map>
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <sstream>
 #include <string>
+#include <cstring>
 #include <algorithm>
+#include <utility>
+#include <cmath>
 #include <limits>
 
 using namespace std;
 
-int N, M;
-int A[101], B[101], cache[101][101];
 const long long NEGINF = numeric_limits<long long>::min();
 
-void set() {
-	for (int i = 0; i < 101; ++i) {
-		A[i] = 0;
-		B[i] = 0;
-		for (int j = 0; j < 101; ++j) {
-			cache[i][j] = -1;
-		}
-	}
-}
+int N, M;
+int arr1[101];
+int arr2[101];
+int cache[101][101];
 
-int JLIS(int idx_A, int idx_B) {
-	int& ret = cache[idx_A + 1][idx_B + 1];
+int JLIS(int idx1, int idx2) {
+	int& ret = cache[idx1 + 1][idx2 + 1];
 	if (ret != -1) return ret;
+	long long num1 = (idx1 == -1) ? NEGINF : arr1[idx1];
+	long long num2 = (idx2 == -1) ? NEGINF : arr2[idx2];
+	long long boundary = max(num1, num2);
 	ret = 2;
-	long long a = idx_A == -1 ? NEGINF : A[idx_A];
-	long long b = idx_B == -1 ? NEGINF : B[idx_B];
-	long long maxElem = max(a, b);
-	for (int next_A = idx_A + 1; next_A < N; ++next_A) {
-		if (A[next_A] > maxElem) {
-			ret = max(ret, 1 + JLIS(next_A, idx_B));
+	for (int i = idx1 + 1; i < N; ++i) {
+		if (arr1[i] > boundary) {
+			ret = max(ret, JLIS(i, idx2) + 1);
 		}
 	}
-	for (int next_B = idx_B + 1; next_B < M; ++next_B) {
-		if (B[next_B] > maxElem) {
-			ret = max(ret, 1 + JLIS(idx_A, next_B));
+	for (int i = idx2 + 1; i < M; ++i) {
+		if (arr2[i] > boundary) {
+			ret = max(ret, JLIS(idx1, i) + 1);
 		}
 	}
 	return ret;
@@ -48,21 +51,19 @@ int JLIS(int idx_A, int idx_B) {
 
 int main()
 {
-	vector<int> answers;
+	vector<int> ans;
 	int cases;
 	cin >> cases;
 	int count = 0;
 	while (count < cases) {
-		set();
 		cin >> N >> M;
-		for (int i = 0; i < N; ++i)
-			cin >> A[i];
-		for (int i = 0; i < M; ++i)
-			cin >> B[i];
-		answers.push_back(JLIS(-1, -1) - 2);
+		for (int i = 0; i < N; ++i) cin >> arr1[i];
+		for (int i = 0; i < M; ++i) cin >> arr2[i];
+		memset(cache, -1, sizeof(cache));
+		ans.push_back(JLIS(-1, -1) - 2);
 		count++;
 	}
-	for (auto i = answers.begin(); i != answers.end(); ++i)
+	for (auto i = ans.begin(); i != ans.end(); ++i)
 		cout << *i << endl;
 	return 0;
 }
