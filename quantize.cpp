139b3c181498c070cded066594b18b34005c460e
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2019-11-05 20:54:22 +0900

    Update quantize.cpp

--- quantize.cpp
+++ quantize.cpp
@@ -5,57 +5,57 @@
 #include <array>
 #include <vector>
 #include <tuple>
+#include <set>
+#include <unordered_set>
 #include <map>
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <sstream>
 #include <string>
+#include <cstring>
 #include <algorithm>
+#include <utility>
 #include <cmath>
 #include <limits>
 
 using namespace std;
 
+#define INF 987654321
 int N, S;
-int num[100];
-int sum_1[100];
-int sum_2[100];
+int arr[101], psum[101], psqsum[101];
+int cache[101][11];
 
 void precalc() {
-	sum_1[0] = num[0];
-	sum_2[0] = num[0] * num[0];
+	sort(arr, arr + N);
+	psum[0] = arr[0];
+	psqsum[0] = arr[0] * arr[0];
 	for (int i = 1; i < N; ++i) {
-		sum_1[i] = sum_1[i - 1] + num[i];
-		sum_2[i] = sum_2[i - 1] + num[i] * num[i];
+		psum[i] = psum[i - 1] + arr[i];
+		psqsum[i] = psqsum[i - 1] + arr[i] * arr[i];
 	}
 }
 
-int minerror(int a, int b) {
-	int sum_1st = (a == 0) ? sum_1[b] - 0 : sum_1[b] - sum_1[a - 1];
-	int sum_2nd = (a == 0) ? sum_2[b] - 0 : sum_2[b] - sum_2[a - 1];
-	int avg = int(0.5 + (double)sum_1st / (b - a + 1));
-	return sum_2nd - 2 * sum_1st * avg + avg * avg * (b - a + 1);
+int minerror(int lo, int hi) {
+	int sum = psum[hi] - (lo == 0 ? 0 : psum[lo - 1]);
+	int sqsum = psqsum[hi] - (lo == 0 ? 0 : psqsum[lo - 1]);
+	int m = int(0.5 + (double)sum / (hi - lo + 1));
+	int ret = sqsum - 2 * m * sum + m * m * (hi - lo + 1);
+	return ret;
 }
 
-int cache[101][10] = { -1, };
-
-void set() {
-	for (int i = 0; i < 101; ++i)
-		for (int j = 0; j < 10; ++j)
-			cache[i][j] = -1;
-}
-
-int Quantize(int from, int parts) {
-	if (from == N)
-		return 0;
-	if (parts == 0)
-		return 987654321;
-	int& res = cache[from][parts];
-	if (res != -1) return res;
-	res = 987654321;
-	for (int len = 1; len <= N - from; ++len) {
-		res = min(res, minerror(from, from + len - 1) + Quantize(from + len, parts - 1));
+int quantize(int from, int parts) {
+	if (from == N) return 0;
+	if (parts == 0) return INF;
+	int& ret = cache[from][parts];
+	if (ret != -1) return ret;
+	ret = INF;
+	for (int part_size = 1; part_size <= N - from; ++part_size) {
+		ret = min(ret, minerror(from, from + part_size - 1) + quantize(from + part_size, parts - 1));
 	}
-	return res;
+	return ret;
 }
-
 
 int main()
 {
@@ -66,11 +66,10 @@ int main()
 	while (count < cases) {
 		cin >> N >> S;
 		for (int i = 0; i < N; ++i)
-			cin >> num[i];
-		sort(num, num + N);
+			cin >> arr[i];
 		precalc();
-		set();
-		ans.push_back(Quantize(0, S));
+		memset(cache, -1, sizeof(cache));
+		ans.push_back(quantize(0, S));
 		count++;
 	}
 	for (auto i = ans.begin(); i != ans.end(); ++i)
