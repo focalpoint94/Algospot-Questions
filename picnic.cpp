d47405cb8b014b38685182a6740bfbb8bb83e2bc
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2020-03-22 20:08:07 +0900

    Update picnic.cpp

--- picnic.cpp
+++ picnic.cpp
@@ -5,87 +5,76 @@
 #include <array>
 #include <vector>
 #include <tuple>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <sstream>
 #include <string>
+#include <cstring>
 #include <algorithm>
+#include <utility>
+#include <cmath>
+#include <limits>
 
 using namespace std;
 
-int answer;
-int taken[10];
+int N; int M;
+int isFriend[10][10];
+int isOccupied[10];
 
-void print_friend_matrix(int **arr, int n) {
-	cout << "Friend Matrix" << endl;
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j)
-			cout << arr[i][j] << " ";
-		cout << endl;
+void set_arr() {
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			isFriend[i][j] = 0;
+		}
+		isOccupied[i] = 0;
 	}
 }
 
-void set_zero() {
-	answer = 0;
-	for (int i = 0; i < 10; ++i)
-		taken[i] = 0;
-}
-
-void count_pairs(int **arr, int n) {
-	int min_idx = -1;
-	for (int i = 0; i < n; ++i) {
-		if (taken[i] == 0) {
-			min_idx = i;
+int solve() {
+	int ans = 0;
+	int min_num = -1;
+	for (int i = 0; i < N; ++i) {
+		if (isOccupied[i] == 0) {
+			min_num = i;
 			break;
 		}
 	}
-	if (min_idx == -1) {
-		answer++;
-		return;
+	if (min_num == -1) {
+		return 1;
 	}
-	for (int i = min_idx + 1; i < n; ++i) {
-		if (arr[min_idx][i] == 1 && taken[i] == 0) {
-			taken[min_idx] = taken[i] = 1;
-			count_pairs(arr, n);
-			taken[min_idx] = taken[i] = 0;
+	for (int i = min_num + 1; i < N; ++i) {
+		if (isFriend[min_num][i] == 1 && isOccupied[i] == 0) {
+			isOccupied[min_num] = isOccupied[i] = 1;
+			ans += solve();
+			isOccupied[min_num] = isOccupied[i] = 0;
 		}
 	}
+	return ans;
 }
 
 int main()
 {
-	vector<int> answers;
+	vector<int> ans;
 	int cases;
 	cin >> cases;
 	int count = 0;
 	while (count < cases) {
-		// n: 학생의 수
-		int n;
-		cin >> n;
-		// pairs: 친구 쌍의 수
-		int pairs;
-		cin >> pairs;
-		// 친구 관계 기록 배열
-		int **arr = new int*[n];
-		for (int i = 0; i < n; ++i)
-			arr[i] = new int[n];
-		for (int i = 0; i < n; ++i)
-			for (int j = 0; j < n; ++j)
-				arr[i][j] = 0;
-		int check = 0;
-		while (check < pairs) {
-			int temp1, temp2;
-			cin >> temp1 >> temp2;
-			arr[temp1][temp2] = 1;
-			arr[temp2][temp1] = 1;
-			check++;
+		set_arr();
+		cin >> N >> M;
+		for (int i = 0; i < M; ++i) {
+			int a, b;
+			cin >> a >> b;
+			isFriend[a][b] = isFriend[b][a] = 1;
 		}
-		set_zero();
-		count_pairs(arr, n);
-		answers.push_back(answer);
-		for (int i = 0; i < n; ++i)
-			delete[] arr[i];
-		delete[] arr;
+		ans.push_back(solve());
 		count++;
 	}
-	for (auto i = answers.begin(); i != answers.end(); ++i)
+	for (auto i = ans.begin(); i != ans.end(); ++i)
 		cout << *i << endl;
 	return 0;
 }
